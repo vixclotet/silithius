439c9081734f5ddd5219a4a57e05939da0ae5e6d
Vix <63135033+vix4723@users.noreply.github.com>
2024-07-02 17:07:38 -0500

    added vercel analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Analytics />
         {assistantId ? children : <Warnings />}
         <img className="logo" src="/openai.svg" alt="OpenAI Logo" />
       </body>
